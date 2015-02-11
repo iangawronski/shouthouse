8f045409839ccd797b2243d7a6c6c7344d277df4
Ian Gawronski <iangawronski@gmail.com>
2015-02-11 10:15:43 -0500

    trying to fix a damn bug...

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, :only => [:new, :create, :destroy, :index] do
-  resources :shouts, :only => [:new, :create, :show, :destroy, :index]
-end
+  get '/users/new', to: 'users#new'
+  put '/users', to: 'users#create'
+
+  root to: "users#index"
+  resources :users, :only => [:index, :destroy] do
+    resources :shouts
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
